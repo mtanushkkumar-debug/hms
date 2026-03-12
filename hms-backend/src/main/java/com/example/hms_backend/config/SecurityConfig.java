340d5ed702539007dda7e908c78b3bf30243323d
tanush-007 <mtanushkkumar@gmail.com>
2026-03-12 09:40:30 +0530

    Update SecurityConfi

--- hms-backend/src/main/java/com/example/hms_backend/config/SecurityConfig.java
+++ hms-backend/src/main/java/com/example/hms_backend/config/SecurityConfig.java
@@ -30,21 +30,25 @@ public class SecurityConfig {
             
         return http.build();
     }
-    @Bean
+     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-            "http://localhost:5173",
-            "http://localhost:5174",
-            "http://localhost:3000",
-            "https://hms-frontend.vercel.app"
+    
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+            "http://localhost:*",
+            "https://*.vercel.app"
         ));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS","PATCH"));
+    
+        configuration.setAllowedMethods(Arrays.asList(
+            "GET","POST","PUT","DELETE","OPTIONS","PATCH"
+        ));
+    
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
     
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
+    
         return source;
     }
 
